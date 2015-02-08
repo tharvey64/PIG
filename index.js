f434e94b5d8736991ae67bf0e5131de5aef0062c
Anthony Keating <anthonyk1225@gmail.com>
2015-02-08 18:24:03 -0500

    fine-tuned stuff

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-//List:
-//Combine some of these fucking Prototypes
-//Like roll button and roll
+
 //Possibly expand game size to more than 2 players
 //add Attributes to PIG: scoreToWin - #OfPlayers - #dieSides
-//currently might have more comments than code
+
 
 function PIG(playerName1, playerName2){
     this.player1 = new Player(playerName1);
     this.player2 = new Player(playerName2);
-    // tracking turn using variables
-    // easiest for 2 player game
+    this.die1 = 0;
+    this.die2 = 0;
     this.current = this.player1;
-    this.next = this.player2
-
+    this.next = this.player2;
     //could use array to track turn
-    //would have to add attribute to player constructor
     //this.plyr = [player1, player2];
     this.turnScore = 0;
 
@@ -42,7 +38,7 @@ PIG.prototype.newRoller = function(){
 
 PIG.prototype.bankButton = function(){
     //WAITING For Selection
-        player.bank(turnScore);
+        this.current.bankScore(this.turnScore);
         this.newRoller();
         this.turnScore = 0;
         return this.next;
@@ -50,29 +46,25 @@ PIG.prototype.bankButton = function(){
 
 PIG.prototype.rollButton = function(){
     //WAITING For Selection
-        var die1 = Math.ceil(Math.random()*6);
-        var die2 = Math.ceil(Math.random()*6);
+        this.die1 = Math.ceil(Math.random()*6);
+        this.die2 = Math.ceil(Math.random()*6);
 
-    if (die1 == 1 && die2 ==1){
+    if (this.die1 == 1 && this.die2 ==1){
         this.current.playerScore = 0;
         this.newRoller();
         return 0;
-    }else if(die1 == 1 || die2 == 1){
+    }else if(this.die1 == 1 || this.die2 == 1){
         this.newRoller();
         return 1;
-    }else if(die1 == die2){
+    }else if(this.die1 == this.die2){
         // player must roll again
-        this.turnScore += (die1 + die2);
+        this.turnScore += (this.die1 + this.die2);
         return 2;
     }else{
-        this.turnScore += (die1 + die2);
+        this.turnScore += (this.die1 + this.die2);
         return 3;
     }//end rollButton else if
-}//end rollButton
-
-// PIG.prototype.roll = function(){
-//     return
-// }//end roll w/ random num generator
+};//end rollButton
 
 // Name pending
 // PIG.prototype.currentData = function(){
@@ -90,6 +82,7 @@ PIG.prototype.rollButton = function(){
 // }//end gameOver
 
 
+
 function Player(name){
     this.name = name;
     this.playerScore = 0;
@@ -99,52 +92,57 @@ function Player(name){
 
 Player.prototype.bankScore = function(score){
     this.playerScore += score;
-}
+};
+
 
 
 $(document).ready(function(){
     //Create form
     //Once player names are entered buttons should be activated
 
-    game = new PIG($('input=[name1]').val(), $('input=[name2]').val());
-
+    // game = new PIG($('input=[name1]').val(), $('input=[name2]').val());
+    game = new PIG("player1", "player2");
 
     $('#roll').on('click', function(){
         var result = game.rollButton(game.current);
         //Insert images before switch statement.
         //Sets die images
-        $('#die1').attr('src', toString(game.die1) + '.png');
-        $('#die1').attr('src', toString(game.die2) + '.png');
 
         // Could also add window messages here
         // Could change the order of the cases
         // or the value returned to clean up switch statement.
+        $('#bank').prop('disabled', false);
         switch(result){
             case 0:
                 // Update player score
-                $('#' + game.next.name).text(game.next.playerScore)
+                var resetScore = game.next.playerScore
+                $('#' + String(game.next.name) + 'Score' + ' p').html(String(resetScore))
                 break;
             case 1:
                 break;
             case 2:
-                // Deactivate Bank Button
+                $('#bank').prop('disabled', true)
                 break;
             case 3:
                 break;
         }//end of roll button switch
 
-        $('#turnScore').text(game.turnScore);
+        console.log(String(game.turnScore));
+        $('#turnScore').html(String(game.turnScore));
+        $('#dieImg1').attr('src','images/' + String(game.die1) + '.png');
+        $('#dieImg2').attr('src','images/' + String(game.die2) + '.png');
     });//end click rollButton
 
     $('#bank').on('click', function(){
         // use this line to update screen
         //I want to insert into the players score not name
-        $('#' + game.current.name).text(game.bankButton(game.current).playerScore);
+        var scoreUpdate = game.bankButton(game.current).playerScore;
+        $('#' + String(game.next.name) + 'Score' + ' p').text(String(scoreUpdate));
 
         //This refers to the score of the player who pressed the button
         // game.next.playerScore;
-
-        $('#turnScore').text(game.turnScore);
+        console.log(game.turnScore);
+        $('#turnScore').text(String(game.turnScore));
 
         //-highlight the new player that is up
     });//end click bankButton
